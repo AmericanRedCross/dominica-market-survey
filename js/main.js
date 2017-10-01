0ae475f18eba31d1123ea695582ed49a710c95c1
Dan Joseph <danbjoseph@gmail.com>
2017-10-01 19:05:07 -0400

    filtering by date

--- js/main.js
+++ js/main.js
@@ -22,7 +22,7 @@ var map = L.map('map', {
 var publicSpreadsheetUrl = 'https://docs.google.com/spreadsheets/d/1Ee2kuKdzqSZ9M62p1OLsaRPIquAKvl_eSDRPf0kO1kI/edit?usp=sharing';
 function init() {
   Tabletop.init( { key: publicSpreadsheetUrl,
-                   callback: showInfo,
+                   callback: drawCalendar,
                    simpleSheet: true } )
 }
 
@@ -59,95 +59,102 @@ function statusColor(status){
   }
 }
 
+var indicator = L.featureGroup();
+
+function clearClicked(cb){
+  if(map.hasLayer(indicator)){ map.removeLayer(indicator); }
+  $('#info-location-name').text('');
+  $('#info-location-type').text('');
+  $('#info-location-status').text('');
+  $('#info-location-notes').text('');
+  $('#info-location-picture').html('');
+  $('#info-location-time').text('');
+  cb();
+}
+
 function onClick(event){
-  var locProps = event.target.options.properties;
-  if(locProps['institution-name'] == "other"){
-    $('#info-location-name').text(locProps['institution-name_other']);
-  } else {
-    $('#info-location-name').text(locProps['institution-name']);
-  }
-  $('#info-location-type').text(locProps['type']);
-  $('#info-location-status').text(locProps['status']);
-  $('#info-location-notes').text(locProps['status-notes']);
+  clearClicked(function(){
+    var locProps = event.target.options.properties;
 
-  var picUrl = "https://arcimagery.s3.amazonaws.com/disasters/2017-dominica-maria/financial_inst_and_market_assessment/" +
-    locProps['picture0'];
-  var picHtml = '<img class="location-pic" src="' + picUrl + '">'
-  $('#info-location-picture').html(picHtml);
-  $('#info-location-time').text(d3.isoParse(locProps.metasubmissionTime))
+    indicator = L.featureGroup().addTo(map);
+    var markerOptions = {
+        radius: 12,
+        fillColor: "#ffffb3",
+        color: "#ffffb3",
+        weight: 3,
+        opacity: 1,
+        fillOpacity: 0.8
+    };
+    var lat = parseFloat(locProps.latitude)
+    var lng = parseFloat(locProps.longitude)
+    var indicatorMarker = L.circleMarker([lat,lng], markerOptions).addTo(indicator);
+    indicatorMarker.bringToBack();
 
+    if(locProps['institution-name'] == "other"){
+      $('#info-location-name').text(locProps['institution-name_other']);
+    } else {
+      $('#info-location-name').text(locProps['institution-name']);
+    }
+    $('#info-location-type').text(locProps['type']);
+    $('#info-location-status').text(locProps['status']);
+    $('#info-location-notes').text(locProps['status-notes']);
+
+    var picUrl = "https://arcimagery.s3.amazonaws.com/disasters/2017-dominica-maria/financial_inst_and_market_assessment/" +
+      locProps['picture0'];
+    var picHtml = '<img class="location-pic" src="' + picUrl + '">'
+    $('#info-location-picture').html(picHtml);
+    $('#info-location-time').text(d3.isoParse(locProps.metasubmissionTime))
+  })
 }
 
 var data;
-function showInfo(table, tabletop){
-  data = table;
-  locations = L.featureGroup();
-  data.forEach(function(d, i) {
-      d.today = d.metasubmissionTime.slice(0,10);
-      var lat = parseFloat(d.latitude)
-      var lng = parseFloat(d.longitude)
-      if(!isNaN(lat) && !isNaN(lng)) {
-        var locationMarker = L.VectorMarkers.icon({
-          icon: awesomeIcon(d.type),
-          markerColor: statusColor(d.status)
-        });
-        L.marker([lat,lng], {
-          icon: locationMarker,
-          properties: d
-        }).on('click', onClick).addTo(locations);
-      }
-  });
-  locations.addTo(map);
-  map.fitBounds(locations.getBounds())
-  doneLoading();
-}
+function drawCalendar(info, tabletop){
 
-function drawCalendar(){
-
-  var calendarColumns = function(month){
-    //expects the month floor e.g. `Tue Mar 01 2016 00:00:00 GMT-0500 (EST)`
-    return d3.time.weeks(d3.time.week.floor(month), d3.time.month.offset(month,1)).length
+  data = info;
+  for(var i=0; i<data.length; i++){
+    data[i].today =  data[i].metasubmissionTime.slice(0,10);
   }
 
-  var minDate = d3.min(calendarData, function(d) { return new Date(d.today) })
-  // var maxDate = new Date("2016-03-28")
-  var maxDate = d3.max(calendarData, function(d) { return new Date(d.today) })
+  var calendarRows = function(month){
+    //expects the month floor e.g. `Tue Mar 01 2016 00:00:00 GMT-0500 (EST)`
+    return d3.timeWeeks(d3.timeWeek.floor(month), d3.timeMonth.offset(month,1)).length
+  }
 
+  var minDate = d3.min(data, function(d) { return new Date(d.metasubmissionTime) })
+  // var maxDate = new Date("2016-03-28")
+  var maxDate = d3.max(data, function(d) { return new Date(d.metasubmissionTime) })
 
  var cellMargin = 2,
      cellSize = 20;
 
- var day = d3.time.format("%w"),
-     week = d3.time.format("%U"),
+ var day = d3.timeFormat("%w"),
+     week = d3.timeFormat("%U"),
      percent = d3.format(".1%"),
-     format = d3.time.format("%Y-%m-%d"),
-     titleFormat = d3.time.format.utc("%a, %d %b");
-     monthName = d3.time.format("%B"),
-     months= d3.time.month.range(d3.time.month.floor(minDate), maxDate);
+     format = d3.timeFormat("%Y-%m-%d"),
+     titleFormat = d3.utcFormat("%a, %d %b");
+     monthName = d3.timeFormat("%B"),
+     months= d3.timeMonth.range(d3.timeMonth.floor(minDate), maxDate);
 
  var svg = d3.select("#calendar").selectAll("svg")
      .data(months)
      .enter().append("svg")
-       .attr("height", (cellSize * 7) + (cellMargin * 8) + 20)
-       .attr("width", function(d) {
-          var columns = calendarColumns(d);
-          return (cellSize * columns) + (cellMargin * (columns + 1));
+       .attr("width", (cellSize * 7) + (cellMargin * 8) )
+       .attr("height", function(d) {
+          var rows = calendarRows(d);
+          return (cellSize * rows) + (cellMargin * (rows + 1)) + 20 ; // the 20 is for the month labels
         })
        .attr("class", "month")
      .append("g")
 
    d3.select("#calendar").selectAll("svg").append("text")
-      .attr("y", (cellSize * 7) + (cellMargin * 8) + 15 )
-      .attr("x", function(d) {
-        var columns = calendarColumns(d);
-        return ((cellSize * columns) + (cellMargin * (columns + 1))) / 2;
-      })
+      .attr("x", ((cellSize * 7) + (cellMargin * 8)) / 2 )
+      .attr("y", 15)
       .attr("class", "month-name")
       .attr("text-anchor", "middle")
       .text(function(d) { return monthName(d); })
 
  var rect = svg.selectAll("rect.day")
-     .data(function(d, i) { return d3.time.days(d, new Date(d.getFullYear(), d.getMonth()+1, 1)); })
+     .data(function(d, i) { return d3.timeDays(d, new Date(d.getFullYear(), d.getMonth()+1, 1)); })
      .enter().append("rect")
        .attr("class", "day")
        .attr("width", cellSize)
@@ -155,8 +162,8 @@ function drawCalendar(){
        .attr("rx", 3)
        .attr("ry", 3)
        .attr("fill", '#eaeaea')
-       .attr("y", function(d) { return (day(d) * cellSize) + (day(d) * cellMargin) + cellMargin; })
-       .attr("x", function(d) { return ((week(d) - week(new Date(d.getFullYear(),d.getMonth(),1))) * cellSize) + ((week(d) - week(new Date(d.getFullYear(),d.getMonth(),1))) * cellMargin) + cellMargin ; })
+       .attr("x", function(d) { return (day(d) * cellSize) + (day(d) * cellMargin) + cellMargin; })
+       .attr("y", function(d) { return ((week(d) - week(new Date(d.getFullYear(),d.getMonth(),1))) * cellSize) + ((week(d) - week(new Date(d.getFullYear(),d.getMonth(),1))) * cellMargin) + cellMargin + 20; })
        .on("mouseover", function(d) {
          d3.select(this).classed('hover', true);
        })
@@ -168,36 +175,84 @@ function drawCalendar(){
  rect.append("title")
     .text(function(d) { return titleFormat(new Date(d)); });
 
-  var data = d3.nest()
+
+  var lookup = d3.nest()
+    .key(function(d) { return d.today; })
+    .rollup(function(leaves) {
+      return leaves.length;
+    })
+    .object(data);
+
+  var count = d3.nest()
     .key(function(d) { return d.today; })
     .rollup(function(leaves) { return leaves.length; })
-    .map(calendarData);
+    .entries(data);
+
+  var scale = d3.scaleLinear()
+    .domain(d3.extent(count, function(d) { return d.value; }))
+    .range([0.4,1]); // the interpolate used for color expects a number in the range [0,1] but i don't want the lightest part of the color scheme
 
 
-    var color = d3.scale.quantize()
-      .domain(d3.extent(calendarData, function(d) { return parseInt(d.count) }))
-      .range(["#9ecae1","#6baed6","#4292c6","#2171b5","#08519c","#08306b"]);
-      // Every ColorBrewer Scale
-      // http://bl.ocks.org/mbostock/raw/5577023/
-
-  rect.filter(function(d) { return d in data; })
-      .style("fill", function(d) { return color(data[d]) })
-      // .classed("clickable", true)
-      // .on("click", function(d){
-      //   if(d3.select(this).classed('focus')){
-      //     d3.select(this).classed('focus', false)
-      //   } else { d3.select(this).classed('focus', true)  }
-      //   filterMap();
-      // })
+  rect.filter(function(d) { return d in lookup; })
+      .style("fill", function(d) { return d3.interpolatePuBu(scale(lookup[d])); })
+      .classed("clickable", true)
+      .on("click", function(d){
+        if(d3.select(this).classed('focus')){
+          d3.select(this).classed('focus', false);
+        } else {
+          d3.select(this).classed('focus', true)
+        }
+        filterMap();
+      })
     .select("title")
-      .text(function(d) { return titleFormat(new Date(d)) + ":  " + data[d]; });
+      .text(function(d) { return titleFormat(new Date(d)) + ":  " + lookup[d]; });
+
+  showInfo();
 
 }
 
-function doneLoading(){
-  console.log("heyu")
+function filterMap(){
+  map.removeLayer(locations);
+  clearClicked(function(){ showInfo(); });
+}
+
+
+
+function showInfo(){
+
+  var dates = [];
+  d3.selectAll("rect.day.focus").each(function(d){
+    dates.push(d)
+  })
+  var filteredData = data;
+  if(dates.length > 0){
+    filteredData = data.filter(function(d){ return dates.indexOf(d.today) !== -1 })
+  }
+
+  locations = L.featureGroup();
+  for(var i=0; i<filteredData.length; i++){
+      var lat = parseFloat(filteredData[i].latitude)
+      var lng = parseFloat(filteredData[i].longitude)
+      if(!isNaN(lat) && !isNaN(lng)) {
+        var locationMarker = L.VectorMarkers.icon({
+          icon: awesomeIcon(filteredData[i].type),
+          markerColor: statusColor(filteredData[i].status)
+        });
+        L.marker([lat,lng], {
+          icon: locationMarker,
+          properties: filteredData[i]
+        }).on('click', onClick).addTo(locations);
+      }
+  }
+  locations.addTo(map);
+  map.fitBounds(locations.getBounds())
+
+
   $("#loading").hide();
 }
+
+
+
 
 // on window resize
 $(window).resize(function(){
